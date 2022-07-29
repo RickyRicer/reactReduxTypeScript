18a7e479aa74236b00bb57f45e0e0ad001773131
Rick Ansay <rick.ansay@gmail.com>
2022-07-29 08:43:48 -0600

    Break out action creators

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore , applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-// import thunk from 'react-thunk';
+import thunk from 'react-thunk';
 import { App } from './components/App';
 import { reducers } from './reducers';
 
-const store = createStore(reducers, applyMiddleware());
+const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
